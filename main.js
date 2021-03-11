adc4dbe5962bf1eed216364aff322cd7b7ba3186
Andy Choe <Andrew.s.choe@gmail.com>
2021-03-11 14:53:17 -0500

    completed assign

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 // Your code here.
+const getFirstName = obj => obj.firstName;
+const getLastName = obj => obj.lastName;
+const getFullName = obj => `${obj.firstName} ${obj.lastName}`
+const setFirstName = (fName, newName) => fName.firstName = newName
+const setAge = (currentAge,newAge) => currentAge.age = newAge
+const giveBirthday = obj => obj.age === undefined ? obj.age = 1 : obj.age++;
 
+const marry = (p1,p2) => {
+  p1.married = true;
+  p2.married = true;
+  p1.spouseName = `${p2.firstName} ${p2.lastName}`;
+  p2.spouseName = `${p1.firstName} ${p1.lastName}`;
+}
 
-
-
+const divorce = (p1, p2) => {
+   p1.married = false;
+  p2.married = false;
+  delete p1.spouseName;
+  delete p2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
